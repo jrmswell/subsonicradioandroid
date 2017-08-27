5af727e10775a635b8113b0ede13b160e10a55f8
James Marsh <cheesytoast@live.co.uk>
2017-08-27 23:46:54 +0100

    Partway through implementing the frames, but hey, the bottom navigation bar is working as intended so I'm happy about that!

--- app/src/main/java/com/jrmswell/subsonicradioandroid/MainActivity.java
+++ app/src/main/java/com/jrmswell/subsonicradioandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.jrmswell.subsonicradioandroid;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -17,66 +19,38 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    //private BottomNavigationView bottomNavigationView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_about:
-                    mTextMessage.setText(R.string.title_about);
-                    return true;
+        public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        transaction.replace(R.id.content,new StationsFragment()).commit();
+                        return true;
+                    case R.id.navigation_about:
+                        transaction.replace(R.id.content,new AboutFragment()).commit();
+                        return true;
+                }
+                return false;
             }
-            return false;
-        }
-
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_main);
 
-        ListView stationsListView = (ListView) findViewById(R.id.stationsListView);
+            BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        HashMap<String, String> radioDesc = new HashMap<>();
-        radioDesc.put("Requests", "A selection of music, chosen by our listeners");
-        radioDesc.put("Background", "All instrumentals, all the time. Listen to a mix of background/area music from the parks");
-        radioDesc.put("Staff Picks", "A selection of tracks picked by our Staff");
-        radioDesc.put("Great Music Ride", "Only Audio of Rides");
-        radioDesc.put("Main Street", "Walk in Uncle Walt's footsteps down Main Street, USA!");
-        radioDesc.put("Wilderness Jct", "Hang out with the bears, camp out near your lodge, or just take the train around to see the sights");
-        radioDesc.put("Future Land", "Welcome to the future -- Future World and Tomorrowland");
-        radioDesc.put("Star Tunnel", "For you Star Tunnel junkies!");
-        radioDesc.put("Soarin'", "For you Soarin' freaks.. Soarin' tracks played over and over and over again");
-        radioDesc.put("Living Seas", "The Living Seas all the time");
-        radioDesc.put("Dole-Whip", "Adventureland, Tiki, Polynesian, Hawaiian");
-        radioDesc.put("Tangaroa", "Have a seat on the Tangaroa Terrace sip your colada or whip, and enjoy the music");
-        radioDesc.put("WEDWay", "Take a ride on the classic WEDWay PeopleMover!");
-
-        List<HashMap<String, String>> listItems = new ArrayList<>();
-        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_item,
-                new String[]{"First Line", "Second Line"},
-                new int[]{R.id.text1, R.id.text2});
-
-        Iterator it = radioDesc.entrySet().iterator();
-        while (it.hasNext()) {
-            HashMap<String, String> resultsMap = new HashMap<>();
-            Map.Entry pair = (Map.Entry) it.next();
-            resultsMap.put("First Line", pair.getKey().toString());
-            resultsMap.put("Second Line", pair.getValue().toString());
-            listItems.add(resultsMap);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.content,new AboutFragment()).commit();
         }
-        stationsListView.setAdapter(adapter);
-
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
-
 }
